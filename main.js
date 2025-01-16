a8ef5d715cc3c01167496423394c9a34c208e609
Cocoa <momiji@maplen.sakura.ne.jp>
2025-01-16 11:51:03 +0900

    数字未入力の値を0に変更

--- main.js
+++ main.js
@@ -5,28 +5,26 @@
     //12桁（小数の場合は9桁）より大きいか、0より小さい数の答えは表示されない（エラーになる）。
     //小数点以下2桁まで表示。たとえば、1.002の場合の1になる。
 
-    let resultNum = undefined; // 上に表示する数字（数字未入力状態ならundefined）
+    let resultNum = 0; // 上に表示する数字（数字未入力状態なら0）
     const numAll = []; // 計算する数字を入れておく配列
     const signAll = []; // 押された符号（+,-,/,* → 1,2,3,4）を入れておく配列
-    let sign = 0; // 連続で符号（+-*/）ボタンを押すのを防止して、最後に押した符号を変更するための変数
-    let previousEqual = 0; // ひとつ前に「=」ボタンを押したことを伝えるための変数
+    let sign = 0; // ひとつ前に符号（+-*/）ボタンを押していたら1。連続で符号ボタンを押すのを防止して、最後に押した符号を変更するための変数
+    let equal = 0; // ひとつ前に「=」ボタンを押したなら1
     let decimal = 0; // 次に押す数字ボタンが小数点以下なら1
-    let decimalPoint0 = 0; // 「.0」を表示するための変数
-    let err = 0; // エラーを表示する場合は1
+    let point0 = 0; // 「.0」を表示するなら1
+    let error = 0; // エラーを表示する場合は1
 
     // 電卓の上の数字を表示
     const drawResult = (resultNum) => {
         if (resultNum < 0 || resultNum >= 1000000000000) {
-            err = 1; // 計算結果が限界値（1兆、13桁）を超えるか、マイナスならエラー
+            error = 1; // 計算結果が限界値（1兆、13桁）を超えるか、マイナスならエラー
         }
 
-        if (err === 1) {
+        if (error === 1) {
             document.querySelector('#result').textContent = 'ERROR'; // エラーと表示
-        } else if (resultNum === undefined) {
-            document.querySelector('#result').textContent = 0; // 数字未入力状態なら0と表示
         } else {
             const num = new Intl.NumberFormat().format(resultNum); // 数字3桁ごとに「,」を表示
-            if (decimalPoint0 === 1) {
+            if (point0 === 1) {
                 document.querySelector('#result').textContent = num + '.0'; // 数字の末尾に「.0」を表示
             } else {
                 document.querySelector('#result').textContent = num;
@@ -36,14 +34,14 @@
 
     // リセット（初期化）
     const reset = () => {
-        resultNum = undefined;
-        previousEqual = 0;
+        resultNum = 0;
+        equal = 0;
         sign = 0;
         decimal = 0; // 小数点以下の数字入力終了
-        decimalPoint0 = 0; // 「.0」の入力無効
+        point0 = 0; // 「.0」の入力無効
         numAll.length = 0; // 数字の配列を空にする
         signAll.length = 0; // 符号の配列を空にする
-        err = 0; // エラーの表記を消す
+        error = 0; // エラーの表示を消す
         drawResult(resultNum); // 電卓の上の数字も初期化
     };
 
@@ -52,32 +50,27 @@
     // 数字ボタンを押した時
     for (let i = 0; i <= 9; i++) {
         document.querySelector('#num' + i).addEventListener('click', () => {
-            // 数字未入力状態なら
-            if (resultNum === undefined) {
-                resultNum = 0;
-            }
-
             // 数字が12桁を超えているか、エラー表示なら
-            if (resultNum.toString().length >= 12 || err === 1) {
+            if (resultNum.toString().length >= 12 || error === 1) {
                 return; // 早期リターン（何もしない）
             }
 
             sign = 0; // 符号を連続で押してないので0に戻す
 
             // ひとつ前に「=」ボタンを押していたら
-            if (previousEqual === 1) {
+            if (equal === 1) {
                 resultNum = 0; // #上の数字を0にする（新たな計算をスタート）
-                previousEqual = 0; // 確認済みなので0に戻す
+                equal = 0; // 確認済みなので0に戻す
             }
 
             // 小数なら（小数点以下は2桁までしか表示しない）
             if (decimal === 1) {
                 if (i >= 1 && i <= 9) {
-                    if (decimalPoint0 === 1 || resultNum.toString().length - Math.trunc(resultNum).toString().length === 2) {
+                    if (point0 === 1 || resultNum.toString().length - Math.trunc(resultNum).toString().length === 2) {
                         // 「.0」か、小数点以下1桁まで表示されているなら
                         resultNum += 0.01*i;
                         resultNum = parseFloat(resultNum.toFixed(2)); // 小数点以下2桁まで表示
-                        decimalPoint0 = 0; // 小数点以下2桁まで入力したので0に戻す
+                        point0 = 0; // 小数点以下2桁まで入力したので0に戻す
                     } else if (resultNum - Math.trunc(resultNum) === 0) {
                         // 整数なら（Math.truncは小数点以下切り捨て）
                         resultNum += 0.1*i;
@@ -85,13 +78,13 @@
                     }
                 } else if (i === 0 && resultNum - Math.trunc(resultNum) === 0) {
                     // 整数で、「.」と「0」が押されたら
-                    decimalPoint0 = 1;
+                    point0 = 1;
                 }
             } else {
                 //整数なら
                 const signAllLast = signAll.length - 1; // 最後に押した符号のインデックス
                 if (i === 0 && signAll[signAllLast] === 4) {
-                    err = 1; // 0で割った場合エラーを出す
+                    error = 1; // 0で割った場合エラーを出す
                 } else if (resultNum >= 1) { 
                     // 前に1以上の数字が押されていたら
                     resultNum = resultNum*10 + i;
@@ -104,25 +97,25 @@
         });
     }
 
-    // 足し算、引き算、掛け算、割り算の符号ボタンを押した時
+    // 足し算、引き算、掛け算、割り算の符号ボタン（idは、+がsign1、-がsign2、×がsign3、÷がsign4）を押した時
     for (let i = 1; i <= 4; i++) {
         document.querySelector('#sign' + i).addEventListener('click', () => {
-            // エラー表示か、数字未入力状態なら
-            if (err === 1 || resultNum === undefined) {
+            // エラー表示なら
+            if (error === 1) {
                 return; // 早期リターン（何もしない）
             }
 
-            previousEqual = 0; // ひとつ前が「=」でも計算を続けるので0に戻す
+            equal = 0; // ひとつ前が「=」でも計算を続けるので0に戻す
             decimal = 0; // 小数点以下の数字入力終了
-            decimalPoint0 = 0; // 「.0」の入力無効
+            point0 = 0; // 「.0」の入力無効
             if (sign === 1) {
                 // ひとつ前に符号ボタンを押していたら、それを新しい符号ボタンで上書きする
                 let m1 = signAll.length - 1;
-                signAll[m1] = i; 
+                signAll[m1] = i;
             } else {
                 numAll.push(resultNum);
                 signAll.push(i);
-                resultNum = undefined;
+                resultNum = 0;
                 drawResult(resultNum);
             }
             sign = 1 // 連続で符号（+-*/）ボタンを押すのを防止して、最後に押した符号を変更するための変数
@@ -133,9 +126,9 @@
     document.querySelector('#equal').addEventListener('click', () => {
         sign = 0; // 符号を連続で押してないので0に戻す
         decimal = 0; // 小数点以下の数字入力終了
-        decimalPoint0 = 0; // 「.0」の入力無効
-        // 符号ボタンを押していないか、エラー表示か、ひとつ前に「=」ボタンを押しているか、数字未入力状態なら
-        if (signAll.length <= 0 || err === 1  || previousEqual === 1 || resultNum === undefined) {
+        point0 = 0; // 「.0」の入力無効
+        // 符号ボタンを押していないか、エラー表示か、ひとつ前に「=」ボタンを押していたら
+        if (signAll.length === 0 || error === 1  || equal === 1) {
             return; // 早期リターン（何もしない）
         }
 
@@ -143,7 +136,7 @@
         let temp = numAll[0]; // 数字の配列の1つ目の数字を仮の変数に代入
 
         for (let i = 0; i < signAll.length; i++) {
-            let i1 = i + 1;
+            const i1 = i + 1;
             if (signAll[i] === 1) {
                 temp += numAll[i1];
             } else if (signAll[i] === 2) {
@@ -151,6 +144,11 @@
             } else if (signAll[i] === 3) {
                 temp *= numAll[i1];
             } else if (signAll[i] === 4) {
+                if (numAll[i1] === 0) {
+                    error = 1; // 0で割った場合はエラーを出す
+                    drawResult(resultNum);
+                    return;
+                }
                 temp /= numAll[i1];
             }
             temp = parseFloat(temp.toFixed(2)); // 小数点以下2桁まで表示
@@ -161,13 +159,13 @@
 
         numAll.length = 0; // 数字の配列を空にする
         signAll.length = 0; // 符号の配列を空にする
-        previousEqual = 1; // ひとつ前に=ボタンを押したことを伝えるための変数
+        equal = 1; // ひとつ前に=ボタンを押したことを伝えるための変数
     });
 
     // 「.」小数点ボタンを押した時
     document.querySelector('#point').addEventListener('click', () => {
-        // 数字未入力状態か、上の数字が10桁以上か、整数でないなら
-        if (resultNum === undefined || resultNum.toString().length >= 10 || resultNum - Math.trunc(resultNum) !== 0) {
+        // 上の数字が10桁以上か、整数でないか、エラー表示なら
+        if (resultNum.toString().length >= 10 || resultNum - Math.trunc(resultNum) !== 0 || error === 1) {
             return; // 何もしない
         }
         sign = 0; // 符号を連続で押してないので0に戻す
@@ -176,30 +174,35 @@
 
     // cボタンを押した時
     document.querySelector('#c').addEventListener('click', () => {
-        resultNum = undefined;
-        previousEqual = 0; // ひとつ前が「=」でも計算を続けるので0に戻す
+        // エラー表示なら
+        if (error === 1) {
+            reset();
+            return; // リセットして以降の処理をしない
+        }
+
+        resultNum = 0;
+        equal = 0; // ひとつ前が「=」でも計算を続けるので0に戻す
         sign = 0; // 符号を連続で押してないので0に戻す
         decimal = 0; // 小数点以下の数字入力終了
-        decimalPoint0 = 0; // 「.0」の入力無効
+        point0 = 0; // 「.0」の入力無効
         drawResult(resultNum);
     });
 
     // →ボタンを押した時
     document.querySelector('#arrow').addEventListener('click', () => {
-        // エラー表示か、「.0」か、数字未入力状態なら
-        if (err === 1 || resultNum === undefined) {
-            reset();
-            return; // リセットして以降の処理をしない
+        // エラー表示か、数字が0なら
+        if (error === 1 || resultNum === 0) {
+            return; // 何もしない
         }
 
-        previousEqual = 0; // ひとつ前が「=」でも計算を続けるので0に戻す
+        equal = 0; // ひとつ前が「=」でも計算を続けるので0に戻す
         sign = 0; // 符号を連続で押してないので0に戻す
         // 上の数字が0か1桁なら
         if (resultNum === 0 || resultNum.toString().length === 1) {
             resultNum = 0;
-        } else if (decimalPoint0 === 1) {
+        } else if (point0 === 1) {
             // 「.0」と表示されていたら、表示を消す
-            decimalPoint0 === 0;
+            point0 === 0;
         } else if (resultNum.toString().length - Math.trunc(resultNum).toString().length === 2) {
             // 小数点以下1桁まで表示されているなら
             resultNum = parseInt(resultNum); // 整数にする
